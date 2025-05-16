40a21d067a3921d58348e5a09a89cfad0551fa88
Jingping Yin <yinjingping2022@gmail.com>
2025-05-16 14:35:16 +0800

    ci: fibo c example

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -4,5 +4,5 @@ if [ -d zkMIPS ]; then
     git -C zkMIPS checkout
     git -C zkMIPS pull --rebase
 else
-    git clone https://github.com/zkMIPS/zkMIPS.git
+    git clone https://github.com/felicityin/zkMIPS.git -b fix-fibo-c-example
 fi
