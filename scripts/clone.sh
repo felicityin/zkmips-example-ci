6b9fb01f0f2fca2b76ecc78194bedeb5f7fc050c
Jingping Yin <yinjingping2022@gmail.com>
2025-06-12 10:00:39 +0800

    chore: clean zkmips

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 if [ -d zkMIPS ]; then
-    git -C zkMIPS checkout
-    git -C zkMIPS pull --rebase
+    # git -C zkMIPS checkout
+    # git -C zkMIPS pull --rebase
+    rm -rf zkMIPS
+    git clone https://github.com/zkMIPS/zkMIPS.git
 else
     git clone https://github.com/zkMIPS/zkMIPS.git
 fi
