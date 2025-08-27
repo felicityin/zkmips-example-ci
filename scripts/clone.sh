4ed36e6a4bc66002c28361add47045c4a60ed1f1
Jingping Yin <yinjingping2022@gmail.com>
2025-08-27 19:26:36 +0800

    test

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -4,7 +4,7 @@ if [ -d zkMIPS ]; then
     # git -C zkMIPS checkout
     # git -C zkMIPS pull --rebase
     rm -rf zkMIPS
-    git clone https://github.com/zkMIPS/zkMIPS.git
+    git clone https://github.com/felicityin/zkMIPS.git -b update-dep
 else
-    git clone https://github.com/zkMIPS/zkMIPS.git
+    git clone https://github.com/felicityin/zkMIPS.git -b update-dep
 fi
